d7beb5619216077e7ac2a19b0f664b5802c3bc00
Raju Bomma <rbomma074@gmail.com>
2026-02-07 18:31:56 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBrx3usV0yDvq2Z3jCQQYQ65qR0EWaP3q4",
-  authDomain: "orin-486608.firebaseapp.com",
-  projectId: "orin-486608",
-  storageBucket: "orin-486608.firebasestorage.app",
-  messagingSenderId: "112825774144",
-  appId: "1:112825774144:web:ba1c24a472eed1307fafa5",
-  measurementId: "G-YMEDSVPCSR"
+  apiKey: "AIzaSyAvYc6zW5GdVDT25MZr_JNAVpdt962zRso",
+  authDomain: "studio-6591899027-af977.firebaseapp.com",
+  projectId: "studio-6591899027-af977",
+  storageBucket: "studio-6591899027-af977.appspot.com",
+  messagingSenderId: "181473633960",
+  appId: "1:181473633960:web:f925f388dd6a50f01c449d"
 };
