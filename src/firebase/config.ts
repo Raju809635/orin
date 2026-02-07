e0400f1dd25951c018053241967d1de023229e40
Raju Bomma <rbomma074@gmail.com>
2026-02-07 17:56:47 +0000

    wait for for firebase internal app i think that outh client ids are not

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,8 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBrx3usV0yDvq2Z3jCQQYQ65qR0EWaP3q4",
   authDomain: "orin-486608.firebaseapp.com",
   projectId: "orin-486608",
-  storageBucket: "orin-486608.appspot.com",
+  storageBucket: "orin-486608.firebasestorage.app",
   messagingSenderId: "112825774144",
-  appId: "1:112825774144:web:ba1c24a472eed1307fafa5"
+  appId: "1:112825774144:web:ba1c24a472eed1307fafa5",
+  measurementId: "G-YMEDSVPCSR"
 };
