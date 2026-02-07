c7a2fb7f69c1f20971079a1dbaa8065b2cb94be2
Raju Bomma <rbomma074@gmail.com>
2026-02-07 17:40:32 +0000

    see i have added auth in console

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,9 +1,3 @@
-// This file is populated with your Firebase configuration.
-export const firebaseConfig = {
-  projectId: "orin-486608",
-  appId: "1:181473633960:web:a31475e7a3a5f9731c449d",
-  apiKey: "AIzaSyAvYc6zW5GdVDT25MZr_JNAVpdt962zRso",
-  authDomain: "orin-486608.firebaseapp.com",
-  storageBucket: "orin-486608.appspot.com",
-  messagingSenderId: "181473633960"
-};
+// This file is deprecated. The correct Firebase config is now located at src/firebase/config.ts
+// This file will be removed in a future update.
+    
